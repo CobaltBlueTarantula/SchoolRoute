b7f1492aea11f71e57c4d2d9d38953e8e6019944
RealFargosMutant <mohammed.alraihan1@education.nsw.gov.au>
2025-08-30 13:31:32 +1000

    added some more theme things

---
+++ src/components/navbar/NavbarContents.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { page } from '$app/stores';
+
+  const tabs = [
+    { name: 'Home', href: '/' },
+    { name: 'Transport', href: '/transport' },
+    { name: 'Dropoff', href: '/dropoff' }
+  ];
+</script>
+
+<div role="tablist" class="tabs tabs-box">
+  {#each tabs as tab}
+    <a
+      role="tab"
+      href={tab.href}
+      class="tab"
+      class:tab-active={$page.url.pathname === tab.href}
+    >
+      {tab.name}
+    </a>
+  {/each}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,8 @@
   import "../app.css";
   import favicon from "$lib/assets/favicon.svg";
 
-  import ThemeSwitcher from "../components/ThemeSwitcher.svelte";
+  import ThemeSwitcher from "../components/navbar/ThemeSwitcher.svelte";
+  import NavbarContents from "../components/navbar/NavbarContents.svelte";
 
   let { children } = $props();
 </script>
@@ -11,10 +12,16 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
+<img
+  src="https://images.unsplash.com/photo-1655841439659-0afc60676b70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  alt="Background"
+  class="fixed inset-0 w-full h-full object-cover z-[-100]"
+/>
+
 <div class="drawer">
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
   <div class="drawer-content flex flex-col">
-    <div class="navbar bg-base-300 w-full">
+    <div class="navbar bg-base-300/80 w-full backdrop-blur-md">
       <div class="flex-none lg:hidden">
         <label
           for="my-drawer-3"
@@ -39,10 +46,7 @@
       <div class="mx-2 flex-1 px-2">Navbar Title</div>
       <div class="hidden flex-none lg:block">
         <ul class="menu menu-horizontal">
-          <!-- Navbar menu content here -->
-          <li><a href="/transport">Home</a></li>
-          <li><a href="/transport">Transport</a></li>
-          <li><a href="/dropoff">Dropoff</a></li>
+          <NavbarContents />
         </ul>
       </div>
       <ThemeSwitcher />
@@ -52,9 +56,7 @@
     <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"
     ></label>
     <ul class="menu bg-base-200 min-h-full w-80 p-4">
-      <li><a href="/">Home</a></li>
-      <li><a href="/transport">Transport</a></li>
-      <li><a href="/dropoff">Dropoff</a></li>
+      <NavbarContents />
     </ul>
   </div>
 </div>
