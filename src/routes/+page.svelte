07becf9e6a82c706af6b2467da32c5a7ee88df94
CobaltBlueTarantula <konstantin.buntsev@education.nsw.gov.au>
2025-08-30 11:56:32 +1000

    Home navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,45 @@
     import "../app.css";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<button class="btn"><a href="/transport">Transport</a></button>
-<button class="btn"><a href="/dropoff">Dropoff</a></button>
+<div class="drawer">
+  <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col">
+    <div class="navbar bg-base-300 w-full">
+      <div class="flex-none lg:hidden">
+        <label for="my-drawer-3" aria-label="open sidebar" class="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            class="inline-block h-6 w-6 stroke-current"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </label>
+      </div>
+      <div class="mx-2 flex-1 px-2">Navbar Title</div>
+      <div class="hidden flex-none lg:block">
+        <ul class="menu menu-horizontal">
+          <!-- Navbar menu content here -->
+          <li><a href="/transport">Transport</a></li>
+          <li><a href="/dropoff">Dropoff</a></li>
+        </ul>
+      </div>
+    </div>
+    
+    <h1>Home Page Content</h1>
+
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
+    <ul class="menu bg-base-200 min-h-full w-80 p-4">
+      <li><a href="/transport">Transport</a></li>
+      <li><a href="/dropoff">Dropoff</a></li>
+    </ul>
+  </div>
+</div>
