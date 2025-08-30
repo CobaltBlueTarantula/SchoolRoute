e9cb87b35ebd3ec3079d245c5a5b8881fcdd30aa
CobaltBlueTarantula <konstantin.buntsev@education.nsw.gov.au>
2025-08-30 11:09:13 +1000

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import "../app.css";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn"><a href="/transport">Transport</a></button>
+<button class="btn"><a href="/dropoff">Dropoff</a></button>
---
+++ src/routes/transport/+page.svelte
@@ -0,0 +1,1 @@
+<h1>This is the transport page</h1>
