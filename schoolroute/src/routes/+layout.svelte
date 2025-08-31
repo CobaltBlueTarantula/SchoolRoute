f08d64173d8e437e34525de4cd5609ae1758d61e
CobaltBlueTarantula <konstantin.buntsev@education.nsw.gov.au>
2025-08-31 15:56:49 +1000

    Make favicon and upload dfd

--- schoolroute/src/routes/+layout.svelte
+++ schoolroute/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
   import "../app.css";
-  import favicon from "$lib/assets/favicon.svg";
+  import favicon from "$lib/assets/favicon.png";
 
   import ThemeSwitcher from "../components/navbar/ThemeSwitcher.svelte";
   import NavbarContents from "../components/navbar/NavbarContents.svelte";
